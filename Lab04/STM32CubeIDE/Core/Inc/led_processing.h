d4c1258611c04d82f3f6dc43726c7898d4dd4c0f
Sonnguyen1402 <ngthaison2001@gmail.com>
2022-11-08 08:35:57 +0700

    add lab04 class

---
+++ Lab04/STM32CubeIDE/Core/Inc/led_processing.h
@@ -0,0 +1,16 @@
+/*
+ * led_processing.h
+ *
+ *  Created on: Oct 26, 2022
+ *      Author: ngtha
+ */
+
+#ifndef INC_LED_PROCESSING_H_
+#define INC_LED_PROCESSING_H_
+
+void updateTrafficLED(int trafficLed0, int trafficLed1);
+void toggleTrafficLed(int trafficLed0, int trafficLed1);
+//void SEG_display();
+void update7SEG(int index7SEG, int counter0, int counter1);
+
+#endif /* INC_LED_PROCESSING_H_ */
